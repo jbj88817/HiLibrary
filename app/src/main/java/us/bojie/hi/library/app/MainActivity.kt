99a7947379471543ee2558a5721dbc61a9076c41
Bojie Jiang <jiangbojie@gmail.com>
2020-06-07 21:22:18 -0700

    2-2-2

--- app/src/main/java/us/bojie/hi/library/app/MainActivity.kt
+++ app/src/main/java/us/bojie/hi/library/app/MainActivity.kt
@@ -4,12 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import us.bojie.hi.library.app.databinding.ActivityMainBinding
 import us.bojie.hi.library.app.demo.HiLogDemoActivity
+import us.bojie.hi.tab.bottom.HiTabBottomInfo
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val homeInfo = HiTabBottomInfo(
+            "首页",
+            "fonts/iconfont.ttf",
+            getString(R.string.if_home),
+            null,
+            "#FF656667",
+            "#FFD44949"
+        )
+        mBinding.tabBottom.setHiTabInfo(homeInfo)
     }
 
     override fun onClick(v: View?) {
